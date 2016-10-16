1b5566915697058e45445800da4294efc7553270
Cr1s19 <alexsis@gmail.com>
2016-10-15 20:37:29 -0500

    update: split

--- Lab_04.cpp
+++ Lab_04.cpp
@@ -29,6 +29,11 @@ int main(){
 	vector<string> v_nodes;
 	vector<string> v_probabilities;
 	vector<string> v_queries;
+
+	vector<string> nodes;
+	vector<string> cases;
+	pair<vector<string>,float> p_cases;
+	vector<pair<vector<string>,float>> probabilities;
 	float p;
 
 while(getline(cin, type)){
@@ -41,11 +46,25 @@ while(getline(cin, type)){
 
 	if (type == "[Probabilities]")
 	{
+		int i = 0;
 		while(getline(cin, probabilitie) && !probabilitie.empty()){
+			
 			cout << probabilitie << "\n";
 			split(probabilitie,'=',v_probabilities);
+			split(v_probabilities[i],'|',cases);
+			
+			if (v_probabilities[i]){
+				
+			}
+
+			p_cases = {cases,v_probabilities.size()-1};
+			cout << "probabilitie: " << v_probabilities[i]<< " ";
+			cout << cases[1] << "\n";
+			probabilities.push_back(p_cases);
+
 			p = stof(v_probabilities[v_probabilities.size()-1]);
 			cout << p << "\n";
+			i += 2;
 		}
 	}
 
