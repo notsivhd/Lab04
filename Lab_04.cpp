3839c1bfa0ca115f6ca0e46961e479c67b7cf41e
Cr1s19 <alexsis@gmail.com>
2016-10-15 19:37:13 -0500

    update: Inicio de split. Conversion de string a float para las probabilidades

--- Lab_04.cpp
+++ Lab_04.cpp
@@ -2,35 +2,58 @@
 #include <cstdio>
 #include <utility>
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+void split(const string& s, char delim,vector<string>& v) {
+    auto i = 0;
+    auto pos = s.find(delim);
+    while (pos != string::npos) {
+      v.push_back(s.substr(i, pos-i));
+      i = ++pos;
+      pos = s.find(delim, pos);
+
+      if (pos == string::npos)
+         v.push_back(s.substr(i, s.length()));
+    }
+}
+
 int main(){
-	
 	// variables
 	string type;
-	string probabilities = " ";
-	string nodes;
-	string queries = " ";
+	string probabilitie = " ";
+	string node;
+	string querie = " ";
+	vector<string> v_nodes;
+	vector<string> v_probabilities;
+	vector<string> v_queries;
+	float p;
 
 while(getline(cin, type)){
 	if (type == "[Nodes]")
 	{
-		getline(cin, nodes);
-		cout << nodes << "\n";
+		getline(cin, node);
+		cout << node << "\n";
+		split(node,'=',v_nodes);
 	}
 
 	if (type == "[Probabilities]")
 	{
-		while(getline(cin, probabilities) && !probabilities.empty()){
-			cout << probabilities << "\n";
+		while(getline(cin, probabilitie) && !probabilitie.empty()){
+			cout << probabilitie << "\n";
+			split(probabilitie,'=',v_probabilities);
+			p = stof(v_probabilities[v_probabilities.size()-1]);
+			cout << p << "\n";
 		}
 	}
 
 	if (type == "[Queries]")
 	{
-		while(getline(cin, queries) && !queries.empty()){
-			cout << queries << "\n";
+		while(getline(cin, querie) && !querie.empty()){
+			cout << querie << "\n";
+			split(querie,'=',v_queries);
 		}
 	}
 }
